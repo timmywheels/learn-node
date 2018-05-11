bcf93e4e53f388310cf21c77b7ec928253ca246c
Tim <tim@timwheeler.com>
2018-05-11 19:49:50 -0400

    project start

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,38 +1,13 @@
 console.log('Starting app.js...');
 
 const fs = require('fs');
-const os = require('os');
 const _ = require('lodash');
+
 const notes = require('./notes.js');
 
-var filteredArray = _.uniq(['Tim', 91,'Tim', 91, 92, 93, 94]);
+var filteredArray = _.uniq(['Tim']);
 
 console.log(filteredArray);
 
 
-
-
-
-
-// console.log(_.isString(true));
-// console.log(_.isString('Tim'));
-
-
-
-// var res = notes.addNote();
-// console.log(res);
-
-// console.log('Result:', notes.add(9, -2));
-
-
-// var user = os.userInfo();
-//
-// console.log(user.username);
-//
-//
-// fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}.` , function(err){
-//     if(err){
-//         console.log('Unable to write to file!');
-//     }
-// });
 
