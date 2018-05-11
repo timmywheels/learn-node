b90673de3c2eae42309c65bbf6c61ff2ea5c0ced
Tim <tim@timwheeler.com>
2018-05-11 18:43:29 -0400

    Started learn-node/

---
+++ notes-node/app.js
@@ -0,0 +1,17 @@
+console.log('Starting app...');
+
+const fs = require('fs');
+const os = require('os');
+
+
+var user = os.userInfo();
+
+console.log(user.username);
+
+
+fs.appendFile('greetings.txt', `Hello ${user.username}!`, function(err){
+    if(err){
+        console.log('Unable to write to file!');
+    }
+});
+
