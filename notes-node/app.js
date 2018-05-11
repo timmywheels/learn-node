fd67896633d1154ef0ef8f0c198052d65d8359d9
Tim <tim@timwheeler.com>
2018-05-11 19:38:46 -0400

    fixed untracked files

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,17 +1,38 @@
-console.log('Starting app...');
+console.log('Starting app.js...');
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
+const notes = require('./notes.js');
+
+var filteredArray = _.uniq(['Tim', 91,'Tim', 91, 92, 93, 94]);
+
+console.log(filteredArray);
 
 
-var user = os.userInfo();
-
-console.log(user.username);
 
 
-fs.appendFile('greetings.txt', `Hello ${user.username}!`, function(err){
-    if(err){
-        console.log('Unable to write to file!');
-    }
-});
+
+
+// console.log(_.isString(true));
+// console.log(_.isString('Tim'));
+
+
+
+// var res = notes.addNote();
+// console.log(res);
+
+// console.log('Result:', notes.add(9, -2));
+
+
+// var user = os.userInfo();
+//
+// console.log(user.username);
+//
+//
+// fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}.` , function(err){
+//     if(err){
+//         console.log('Unable to write to file!');
+//     }
+// });
 
