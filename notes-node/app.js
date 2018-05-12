0d4751246408c78d1fc7d2b7b170e1a929e71a5a
Tim <tim@timwheeler.com>
2018-05-11 22:40:13 -0400

    updated project

--- notes-node/app.js
+++ notes-node/app.js
@@ -15,12 +15,16 @@ console.log('Yargs:', argv);
 
 if(command === 'add'){
     notes.addNote(argv.title, argv.body);
+
 } else if(command === 'list'){
     notes.getAll();
+
 } else if(command === 'read'){
     notes.getNote(argv.title);
+
 } else if(command === 'remove') {
     notes.removeNote(argv.title);
+    
 } else {
     console.log('Command not recognized!');
 }
